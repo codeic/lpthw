ffa8c44992576039b281226186c14ff3e979df69
Igor <iv.codeic@gmail.com>
2018-04-21 07:24:36 +0200

    New or modified files.

--- ex16.py
+++ ex16.py
@@ -24,12 +24,14 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + "\n" + line2 + "\n" + line3)
+
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print "And finally, we close it."
 target.close()
@@ -51,9 +53,11 @@ Or you can do 'more test.txt' or 'less test.txt' or 'cat test.txt' but
 that's not the point anyway....
 """
 
-# 3. There’s too much repetition in this fi le. Use strings, formats, 
+# 3. There’s too much repetition in this file. Use strings, formats, 
 # and escapes to print out line1, line2, and line3 with just one 
 # target.write() command instead of six.
+    # A: It took me nearly a half an hour to figure this out. I didn't
+    # used formats, though.
 
 # 4. Find out why we had to pass a 'w' as an extra parameter to open. 
 # Hint: open tries to be safe by making you explicitly say you want to 
