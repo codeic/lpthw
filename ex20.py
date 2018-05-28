a29009d7526bc184e48a88a79ffbfbecad412f28
codeic <iv.codeic@gmail.com>
2018-05-28 07:03:40 +0200

    Commit.

--- ex20.py
+++ ex20.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 *-*
+
 # This line imports *variable* from a module.
 # (https://stackoverflow.com/a/46810537/4455055)
 from sys import argv
@@ -12,35 +14,36 @@ def print_all(f):
 # This line reads value given to parameter and prints it out.
     print f.read()
 
-# This line defines function with one parameter, same as in previous
-# function.
+# This line defines function with one parameter, same as in the previous
+# function. 
 def rewind(f):
-# This line seeks for reference point ('f') which is the beginning of the file, since '0' is given as 'from_what' parameter.
+# This line seeks for reference point ('f') which is the beginning of 
+# the file, since '0' is given as 'from_what' parameter.
 # (https://stackoverflow.com/a/11696554/4455055)
     f.seek(0)
 
 # This line defines function with one new and one parameter from 
 # previous functions.
 def print_a_line(line_count, f):
-# This line prints first parameter, then reads one line of second 
-# parameter.
+# This line prints first parameter, then prints out readings of one 
+# line of the second parameter.
     print line_count, f.readline()
 
 # This line assings value to variable - opening of a given paramater.
 current_file = open(input_file)
 
-# This line prints sentence.
+# This line prints a sentence.
 print "First let's print the whole file:\n"
 # This line calls function with variable as a given parameter.
 print_all(current_file)
 
-# This line prints sentence.
+# This line prints a sentence.
 print "Now let's rewind, kind of like a tape."
 
 # This line calls function with variable as a given parameter.
 rewind(current_file)
 
-# This line prints sentence.
+# This line prints a sentence.
 print "Let's print three lines:"
 
 # This line assigns value to variable.
@@ -49,29 +52,39 @@ current_line = 1
 print_a_line(current_line, current_file)
 
 # This line assigns value to variable and increments it.
-current_line = current_line + 1
+current_line += 1
 # This line calls function and gives it two variables as parameters.
 print_a_line(current_line, current_file)
 
-# This line assigns value to variable and increments it.
-current_line = current_line + 1
+# This line assigns value to variable and increments it further.
+current_line += 1
 # This line calls function and gives it two variables as parameters.
 print_a_line(current_line, current_file)
 
 # Study Drills
 
 # 1. Go through and write English comments for each line to understand 
-# what's going on.
+# what's going on. ✔
 
 # 2. Each time print_a_line is run you are passing in a variable 
 # current_line. Write out what current_line is equal to on each 
 # function call, and trace how it becomes line_count in print_a_line.
+    # A:
+    # First current_line is equal to 1. Later on, it is incremented
+    # every time for 1. It is same as line_count because it is 
+    # called as a part of a function (print_a_line) and it serves it
+    # as a first parameter.
 
 # 3. Find each place a function is used, and go check its def to make 
 # sure that you are giving it the right arguments.
+    # A:
+    # Doesn't look like anything to me.
 
 # 4. Research online what the seek function for file does. Try pydoc 
 # file and see if you can figure it out from there.
+    # A:
+    # The answer is already written where the line with seek function
+    # is explained.
 
 # 5. Research the shorthand notation += and rewrite the script to use 
-# that.
+# that. ✔
